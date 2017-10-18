4eb987d07b912320656d479abad5a6dac7a3341a
Thesoreon <pavels.sus@gmail.com>
2017-10-18 16:21:22 +0200

    - Added Readme info
    - Added javascript code, that fixes hiding navigation in small
    resolution.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Website
+Hi my name is Paul and this is my first project shared on github.
+I have worked with bitbucket for few months, but it came the time to change it!
+
+This is my project, that im using to learn basics of html, css, javascript, php and more.
+
+My first experiences with websites languages were when i was fourteen.
+I'm making a lot of projects. I'm trying to make games with Unity and C#. (about one year, started 2k16 October)
--- script.js
+++ script.js
@@ -3,14 +3,26 @@ function closeMenu() {
 
 	if(isActive) {
     	document.getElementById("navigation").style.visibility = "hidden";
+    	document.body.style.paddingTop = "2em";
 	}
 	else {
     	document.getElementById("navigation").style.visibility = "visible";
+    	document.body.style.paddingTop = "17.4em";
 	}
 
 	isActive = !isActive;
 }
 
 window.onresize = function() {
-    document.getElementById("navigation").style.visibility = "visible";
+
+	if(window.innerWidth > 600)
+	{
+    	document.getElementById("navigation").style.visibility = "visible";
+    	document.body.style.paddingTop = "50px";
+    }
+    else
+    {
+    	document.getElementById("navigation").style.visibility = "visible";
+    	document.body.style.paddingTop = "17.4em";
+    }
 };
